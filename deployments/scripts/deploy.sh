6a7f398c19969c7a1ee583403689519bf1b87147
Wilian Gabriel <wilian746@hotmail.com>
2020-06-13 10:49:38 -0300

    Change deploy (#14)

--- deployments/scripts/deploy.sh
+++ deployments/scripts/deploy.sh
@@ -7,7 +7,13 @@ if [ "$VERSION" = "" ]; then
 fi
 
 chmod +x "./deployments/scripts/setup_version.sh"
-sh "./deployments/scripts/setup_version.sh"
+"./deployments/scripts/setup_version.sh"
+
+grep -q "{{VERSION_NOT_FOUND}}" "./internal/commands/version/version.go"
+if [ $? -eq 0 ]; then
+  echo "The version has not been changed, check and try again!"
+  exit 1
+fi
 
 git tag $VERSION
 if [ $? -eq 0 ]; then
